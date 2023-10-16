69ae22db0db69e9600eb323ca3cacd16398142d5
Dmitry <dmitry2595@gmail.com>
2023-10-16 20:59:02 +0300

    feat: start project

---
+++ src/components/List.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function List() {
+  return (
+    <div className='content'>
+        <div className='list'>
+            List
+        </div>
+    </div>
+  )
+}
