9a612ba1fed4c9e9770d62955d33040aa631a0d9
LetovD <dmitry2595@gmail.com>
2023-10-17 11:48:05 +0300

    feat: banner frame

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,7 +4,15 @@ export default function Banner() {
   return (
     <div className='content'>
         <div className='banner'>
-            Banner
+            <div className='header'>Navigation</div>
+            <div className='banner-content'>
+              <div>
+                <h2>Banner title</h2>
+                <p>Banner subtitle</p>
+              </div>
+              <button className='banner-button'>Click me!</button>
+            </div>
+            <div className='banner-arrow-down'>&#129131;</div>
         </div>
     </div>
   )
