44790ec41a430fe4e6615567e718e1a4ab1dd455
LetovD <dmitry2595@gmail.com>
2023-10-18 17:06:44 +0300

    feat: edit tickets component

--- src/components/ChooseMovie.js
+++ src/components/ChooseMovie.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function ChooseMovie({movies}) {
-    const moviesList = movies.map(m => <button className='choose-movie-btn'>{m}</button>)
+export default function ChooseMovie({movies, changeMovie}) {
+    const [ind, setInd] = useState()
+
+    function changeInd(i) {
+        setInd(i)
+        changeMovie(i)
+    }
+
+    function ChooseMovieButton({ children, isActive, onClick }) {
+        if (isActive) {
+          return <button className='choose-movie-btn active'>{children}</button>
+        }
+        return (
+          <button className='choose-movie-btn' onClick={() => {
+            onClick();
+          }}>
+            {children}
+          </button>
+        )
+      }
+
+    // const moviesList = movies.map(m => <button className='choose-movie-btn' onClick={() => {changeInd('dr'), className='choose-movie-btn active'}}>{m}</button>)
+    const moviesList = movies.map(m => <ChooseMovieButton 
+        children={m}
+        isActive={m.index}
+        onClick={() => {changeInd('dr')}}
+        />)
     return (
         <div className='choose-movie'>
             <h3>Choose movie:</h3>
             <div>{moviesList}</div>
+            <p>{ind}</p>
         </div>
   )
 }
--- src/components/Tickets.js
+++ src/components/Tickets.js
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ChooseMovie from './ChooseMovie'
 import ChooseTime from './ChooseTime'
 
 
 export default function Tickets({movies, sessions}) {
-  let today = new Date().toISOString().slice(0, 10)
+  const [currentMovie, setCurrentMovie] = useState()
+
+  function changeCurrentMovie(movieInd){
+    setCurrentMovie(movies[movieInd])
+  }
+
   return (
     <div className='content'>
         <div className='tickets'>
           <h1>Tickets</h1>
-          <ChooseMovie movies={movies}/>
+          <ChooseMovie movies={movies} changeMovie={changeCurrentMovie}/>
           <ChooseTime  sessions={null}/>
           {/* <ChooseSeat /> */}
         List of movies: {movies}
